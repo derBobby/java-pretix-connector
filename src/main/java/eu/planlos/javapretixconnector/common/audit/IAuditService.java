df7273d54631a43c17819d1a0353a27699733072
Sascha Schmitt <sascha.schmitt@enbw.com>
2023-12-12 00:41:05 +0100

    Removing Audit functions

--- src/main/java/eu/planlos/javapretixconnector/common/audit/IAuditService.java
+++
@@ -1,9 +0,0 @@
-package eu.planlos.javapretixconnector.common.audit;
-
-import java.time.ZonedDateTime;
-import java.util.List;
-
-public interface IAuditService {
-    List<AuditEntry> getAfter(ZonedDateTime zdt);
-    List<AuditEntry> getAll();
-}
