84d9f06325ed51424ee2abebaa794ec18c44b4c7
derBobby <dev@planlos.eu>
2023-12-23 14:32:45 +0100

    Add description to PretixSupportedActions.java

--- src/main/java/eu/planlos/javapretixconnector/model/dto/PretixSupportedActions.java
+++ src/main/java/eu/planlos/javapretixconnector/model/dto/PretixSupportedActions.java
@@ -7,15 +7,17 @@ import java.util.Arrays;
 
 @Getter
 public enum PretixSupportedActions {
-    ORDER_PLACED("pretix.event.order.placed"),
-    ORDER_NEED_APPROVAL("pretix.event.order.placed.require_approval"),
-    ORDER_APPROVED("pretix.event.order.approved"),
-    ORDER_CANCELED("pretix.event.order.canceled");
+    ORDER_PLACED("pretix.event.order.placed", "New"),
+    ORDER_NEED_APPROVAL("pretix.event.order.placed.require_approval", "Need OK"),
+    ORDER_APPROVED("pretix.event.order.approved", "Got OK"),
+    ORDER_CANCELED("pretix.event.order.canceled", "Cancelled");
 
     private final String action;
+    private final String description;
 
-    PretixSupportedActions(String action) {
+    PretixSupportedActions(String action, String description) {
         this.action = action;
+        this.description = description;
     }
 
     public static boolean isSupportedAction(String actionString) {
