b9160af185b2104a443fc2cb2f6fa380f222c02a
derBobby <dev@planlos.eu>
2023-12-27 17:50:00 +0100

    v1.1.0-SNAPSHOT
    - Added logging
    - Added configuration handling on startup

---
+++ src/main/java/eu/planlos/javapretixconnector/Application.java
@@ -0,0 +1,23 @@
+package eu.planlos.javapretixconnector;
+
+import eu.planlos.javapretixconnector.model.dto.PretixSupportedActions;
+import eu.planlos.javapretixconnector.model.dto.WebHookResult;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Profile;
+
+@Profile("DEV")
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class Application implements IPretixWebHookHandler{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public WebHookResult handleWebhook(String organizer, String event, String code, PretixSupportedActions action) {
+        return null;
+    }
+}
