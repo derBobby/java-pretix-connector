d3ce99b1db1f0557583108f6e28737b9d02779be
derBobby <dev@planlos.eu>
2024-05-18 23:50:57 +0200

    Show error if invalid

--- src/main/java/eu/planlos/javapretixconnector/model/dto/PretixEventFilterCreateDTO.java
+++ src/main/java/eu/planlos/javapretixconnector/model/dto/PretixEventFilterCreateDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.planlos.javapretixconnector.model.validation.ValidAction;
 import eu.planlos.javapretixconnector.model.validation.ValidEvent;
 import eu.planlos.javapretixconnector.model.validation.ValidFilterMap;
+import eu.planlos.javapretixconnector.model.validation.ValidOrganizer;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.Map;
 
 public record PretixEventFilterCreateDTO(
     @JsonProperty("action") @NotNull @ValidAction String action,
-    @JsonProperty("organizer") @NotNull @ValidEvent String organizer,
+    @JsonProperty("organizer") @NotNull @ValidOrganizer String organizer,
     @JsonProperty("event") @NotNull @ValidEvent String event,
     @JsonProperty("qna-list") @NotNull @ValidFilterMap Map<String, List<String>> filterMap){
 }
