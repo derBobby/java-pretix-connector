86fcfa178fea6b1b65f2b2e6a8126b16c12108b0
derBobby <dev@planlos.eu>
2024-05-20 23:05:34 +0200

    Action populated by thymeleaf

--- src/main/java/eu/planlos/javapretixconnector/controller/PretixEventFilterController.java
+++ src/main/java/eu/planlos/javapretixconnector/controller/PretixEventFilterController.java
@@ -1,13 +1,16 @@
 package eu.planlos.javapretixconnector.controller;
 
+import eu.planlos.javapretixconnector.model.dto.PretixSupportedActions;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PretixEventFilterController {
 
     @GetMapping("/pretixconnector/eventfilters")
-    public String getAllEvents() {
+    public String getAllEvents(Model model) {
+        model.addAttribute("pretixSupportedActions", PretixSupportedActions.values());
         return "event-filters";
     }
 }
